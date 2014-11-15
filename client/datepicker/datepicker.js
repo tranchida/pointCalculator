f67aa162dde21e65c119072e404371b701e71c77
tranchida <giampaolo@tranchida.ch>
2014-11-15 06:45:50 +0000

    add localized jquery-ui datepicker fr-CH change jquery-ui source package

---
+++ client/datepicker/datepicker.js
@@ -0,0 +1,10 @@
+/**
+ * Created with PointCalculator.
+ * User: tranchida
+ * Date: 2014-11-13
+ * Time: 04:55 AM
+ * To change this template use Tools | Templates.
+ */
+Template.datepicker.rendered = function() {
+    $("#datepicker").datepicker($.datepicker.regional["fr-CH"]);
+};
