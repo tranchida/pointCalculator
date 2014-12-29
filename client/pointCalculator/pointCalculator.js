186c3d970444ff5ae28cd3966948bc33f54be04a
tranchida <giampaolo@tranchida.ch>
2014-12-29 11:17:37 +0000

    remove not necessary

--- client/pointCalculator/pointCalculator.js
+++ client/pointCalculator/pointCalculator.js
@@ -3,12 +3,12 @@ Session.setDefault('poid', 100);
 
 Template.pointCalculator.events({
     'input .nutriment': function(e) {
-        var proteins = e.target.form.proteins.value;
-        var glucides = e.target.form.glucides.value;
-        var lipides = e.target.form.lipides.value;
-        var fibres = e.target.form.fibres.value;
-        var poid = e.target.form.poid.value;
-        var points = Math.round((proteins / 11 + glucides / 9 + lipides / 4 + fibres / 30) * poid) / 100;
+        proteins = e.target.form.proteins.value;
+        glucides = e.target.form.glucides.value;
+        lipides = e.target.form.lipides.value;
+        fibres = e.target.form.fibres.value;
+        poid = e.target.form.poid.value;
+        points = Math.round((proteins / 11 + glucides / 9 + lipides / 4 + fibres / 30) * poid) / 100;
         Session.set("poid", poid);
         Session.set('points', points);
     }
